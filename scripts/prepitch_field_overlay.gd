8ff3fd8be020780df1c4525956ea7728eaa28b39
Collin Erickson <collinberickson@gmail.com>
2025-01-08 23:51:00 -0500

    Minor improvements to prepitch overlay

--- scripts/prepitch_field_overlay.gd
+++ scripts/prepitch_field_overlay.gd
@@ -1,16 +1,17 @@
 extends Control
 
 func setup_pitcher(player) -> void:
-	var vbox = $Pitcher/MarginContainer/VBoxContainer
-	vbox.get_node("Line1").text = player.first
-	vbox.get_node("Line2").text = player.last
-	vbox.get_node("Line3").text = ""
+	var vbox = $Pitcher/MarginContainer/MarginContainer/VBoxContainer
+	vbox.get_node("Line1").text = player.first + " " + player.last
+	vbox.get_node("Line2").text = ""
+	vbox.get_node("Line3").text = "Pitching: " + str(player.pitching)
 	return
 
 
 func setup_batter(player) -> void:
-	var vbox = $Batter/MarginContainer/VBoxContainer
-	vbox.get_node("Line1").text = player.first
-	vbox.get_node("Line2").text = player.last
-	vbox.get_node("Line3").text = ""
+	var vbox = $Batter/MarginContainer/MarginContainer/VBoxContainer
+	vbox.get_node("Line1").text = player.first + " " + player.last
+	vbox.get_node("Line2").text = "Contact: " + str(player.contact)
+	vbox.get_node("Line3").text = "Speed: " + str(player.speed)
+	#$Batter.size.x = $Batter/MarginContainer/MarginContainer/VBoxContainer.size.x
 	return
